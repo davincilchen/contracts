517aa2054fb9f6eeb72dd44bbdd67e5b484dd7c5
Juin Chiu <juin.chiu@infinitechain.io>
2018-05-23 18:39:15 +0800

    Use new data model

--- gringotts/contracts/SidechainLib.sol
+++ gringotts/contracts/SidechainLib.sol
@@ -62,46 +62,52 @@ contract SidechainLib {
         ===lightTxData===
         _parameter[1] = _from
         _parameter[2] = _to
-        _parameter[3] = _value
-        _parameter[4] = _fee
-        _parameter[5] = _lsn
+        _parameter[3] = _assetID
+        _parameter[4] = _value
+        _parameter[5] = _fee
+        _parameter[6] = _nonce
+        _parameter[7] = _logID
+        _parameter[8] = _metadataHash
 
         ===clientLtxSignature===
-        _parameter[6] = _vFromClient
-        _parameter[7] = _rFromClient
-        _parameter[8] = _sFromClient
+        _parameter[9] = _vFromClient
+        _parameter[10] = _rFromClient
+        _parameter[11] = _sFromClient
 
         ===receipt===
-        _parameter[9] = _gsn,
-        _parameter[10] = _fromBalance,
-        _parameter[11] = _toBalance,
+        _parameter[12] = _gsn,
+        _parameter[13] = _fromBalance,
+        _parameter[14] = _toBalance,
 
         ===serverLtxSignature===
-        _parameter[12] = _vFromServer
-        _parameter[13] = _rFromServer
-        _parameter[14] = _sFromServer
+        _parameter[15] = _vFromServer
+        _parameter[16] = _rFromServer
+        _parameter[17] = _sFromServer
 
         ===serverReceiptSignature===
-        _parameter[15] = _vFromServer,
-        _parameter[16] = _rFromServer,
-        _parameter[17] = _sFromServer,
+        _parameter[18] = _vFromServer,
+        _parameter[19] = _rFromServer,
+        _parameter[20] = _sFromServer,
 
-        _parameter[18] = _dsn (deposit)
+        // _parameter[21] = _dsn (deposit)
         */
-        bytes32[] memory bytes32Array = new bytes32[](5);
+        bytes32[] memory bytes32Array = new bytes32[](8);
         bytes32Array[0] = _parameter[1]; // from
         bytes32Array[1] = _parameter[2]; // to
-        bytes32Array[2] = _parameter[3]; // value
-        bytes32Array[3] = _parameter[4]; // fee
-        bytes32Array[4] = _parameter[5]; // lsn
+        bytes32Array[2] = _parameter[3]; // assetID
+        bytes32Array[3] = _parameter[4]; // value
+        bytes32Array[4] = _parameter[5]; // fee
+        bytes32Array[5] = _parameter[6]; // nonce
+        bytes32Array[6] = _parameter[7]; // logID
+        bytes32Array[7] = _parameter[8]; // metadataHash
         bytes32 hashMsg = hashArray(bytes32Array);
         require(hashMsg == _parameter[0]);
-        address signer = verify(hashMsg, uint8(_parameter[6]), _parameter[7], _parameter[8]);
+        address signer = verify(hashMsg, uint8(_parameter[9]), _parameter[10], _parameter[11]);
         if (address(0) == address(_parameter[1]) && address(0) != address(_parameter[2])) {
             // Deposit
             require(signer == address(_parameter[2]));
-            require(signer == address(depositLogs[_parameter[18]].client));
-            require(_parameter[3] == depositLogs[_parameter[18]].value);
+            require(signer == address(depositLogs[_parameter[7]].client));
+            require(_parameter[4] == depositLogs[_parameter[7]].value);
         } else if (address(0) != address(_parameter[1]) && address(0) == address(_parameter[2])) {
             // Withdraw
             require(signer == address(_parameter[1]));
@@ -109,13 +115,14 @@ contract SidechainLib {
             revert();
         }
 
+        bytes32Array = new bytes32[](5);
         bytes32Array[0] = bytes32(stageHeight+1); // stageHeight
-        bytes32Array[1] = _parameter[9];          // gsn
+        bytes32Array[1] = _parameter[12];         // gsn
         bytes32Array[2] = _parameter[0];          // lightTxHash
-        bytes32Array[3] = _parameter[10];         // fromBalance
-        bytes32Array[4] = _parameter[11];         // toBalance
+        bytes32Array[3] = _parameter[13];         // fromBalance
+        bytes32Array[4] = _parameter[14];         // toBalance
         hashMsg = hashArray(bytes32Array);
-        signer = verify(hashMsg, uint8(_parameter[15]), _parameter[16], _parameter[17]);
+        signer = verify(hashMsg, uint8(_parameter[18]), _parameter[19], _parameter[20]);
         require (signer == owner);
         _;
     }
@@ -221,19 +228,19 @@ contract SidechainLib {
     }
 
     function deposit (bytes32[] _parameter) isSigValid (_parameter) public onlyOwner {
-        depositLogs[_parameter[18]].flag = true;
+        depositLogs[_parameter[7]].flag = true;
 
         emit VerifyReceipt ( 0,
-                             _parameter[9],
+                             _parameter[12],
                              _parameter[0],
-                             _parameter[10],
-                             _parameter[11],
-                             [ _parameter[12],
-                               _parameter[13],
-                               _parameter[14]],
+                             _parameter[13],
+                             _parameter[14],
                              [ _parameter[15],
                                _parameter[16],
-                               _parameter[17]]);
+                               _parameter[17]],
+                             [ _parameter[18],
+                               _parameter[19],
+                               _parameter[20]]);
     }
 
     function proposeWithdrawal (bytes32[] _parameter) isSigValid (_parameter) public {
@@ -241,25 +248,25 @@ contract SidechainLib {
         wsn = concat(stageHeight + gsn)
         */
         bytes32[] memory bytes32Array = new bytes32[](2);
-        bytes32Array[0] = bytes32(stageHeight+1);
-        bytes32Array[1] = _parameter[9];
+        bytes32Array[0] = _parameter[1]; // from
+        bytes32Array[1] = _parameter[6]; // nonce
         bytes32 wsn = hashArray(bytes32Array);
 
         withdrawalLogs[wsn].stage = bytes32(stageHeight+1);
         withdrawalLogs[wsn].client = _parameter[1];
-        withdrawalLogs[wsn].value = bytes32(uint256(_parameter[3]) - uint256(_parameter[4])); // value - fee
+        withdrawalLogs[wsn].value = bytes32(uint256(_parameter[4]) - uint256(_parameter[5])); // value - fee
 
         emit VerifyReceipt ( 1,
-                             _parameter[9],
+                             _parameter[12],
                              _parameter[0],
-                             _parameter[10],
-                             _parameter[11],
-                             [ _parameter[12],
-                               _parameter[13],
-                               _parameter[14]],
+                             _parameter[13],
+                             _parameter[14],
                              [ _parameter[15],
                                _parameter[16],
-                               _parameter[17]]);
+                               _parameter[17]],
+                             [ _parameter[18],
+                               _parameter[19],
+                               _parameter[20]]);
     }
 
     function withdraw (bytes32[] _parameter) public {
@@ -280,32 +287,32 @@ contract SidechainLib {
 
     function instantWithdraw (bytes32[] _parameter) isSigValid (_parameter) public {
         // instantWithdraw condition
-        require (uint256(_parameter[3]) <= 10000000000000000000);
+        require (uint256(_parameter[4]) <= 10 ether);
         /*
-        wsn = concat((stageHeight+1) + gsn)
+        wsn = concat(from + nonce)
         */
         bytes32[] memory bytes32Array = new bytes32[](2);
-        bytes32Array[0] = bytes32(stageHeight+1);
-        bytes32Array[1] = _parameter[9];
+        bytes32Array[0] = _parameter[1]; // from
+        bytes32Array[1] = _parameter[6]; // nonce
         bytes32 wsn = hashArray(bytes32Array);
 
         withdrawalLogs[wsn].stage = bytes32(stageHeight+1);
         withdrawalLogs[wsn].client = _parameter[1];
-        withdrawalLogs[wsn].value = _parameter[3];
+        withdrawalLogs[wsn].value = _parameter[4];
         withdrawalLogs[wsn].flag = true;
 
-        address(_parameter[2]).transfer(uint256(_parameter[3]));
+        address(_parameter[2]).transfer(uint256(_parameter[4]));
 
         emit VerifyReceipt ( 2,
-                             _parameter[9],
+                             _parameter[12],
                              _parameter[0],
-                             _parameter[10],
-                             _parameter[11],
-                             [ _parameter[12],
-                               _parameter[13],
-                               _parameter[14]],
+                             _parameter[13],
+                             _parameter[14],
                              [ _parameter[15],
                                _parameter[16],
-                               _parameter[17]]);
+                               _parameter[17]],
+                             [ _parameter[18],
+                               _parameter[19],
+                               _parameter[20]]);
     }
 }
