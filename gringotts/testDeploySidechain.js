29c2356423f68d73a9b6a03e9c708f1e511b96a0
ming.hsu <ming.hsu@isuncloud.com>
2018-07-02 17:12:27 +0800

    Add timeout

--- gringotts/testDeploySidechain.js
+++ gringotts/testDeploySidechain.js
@@ -6,6 +6,15 @@ let web3Url = 'http://' + env.web3Host + ':' + env.web3Port;
 let web3 = new Web3(new Web3.providers.HttpProvider(web3Url));
 let im = web3.eth.contract(InfinitechainManager.abi).at(argv.managerAddress);
 let sidechainNumber = im.sidechainNumber().toNumber();
-im.deploySidechain(argv.sidechainOwner, ['0x0'], web3.toWei(10), { from: env.account, gas: 600000 });
-while (im.sidechainAddress(sidechainNumber) == '0x0000000000000000000000000000000000000000') {}
-console.log(im.sidechainAddress(sidechainNumber));
+im.deploySidechain(argv.sidechainOwner, ['0x0'], web3.toWei(10), { from: env.account, gas: 1000000 });
+let second = 0;
+let timeout = 120;
+let countSecond = () => {
+    second = second + 1;
+    t = setTimeout(countSecond, 1000);
+    if (second >= timeout || im.sidechainAddress(sidechainNumber) != '0x0000000000000000000000000000000000000000') {
+        console.log(im.sidechainAddress(sidechainNumber));
+        clearTimeout(t);
+    }
+}
+countSecond();
