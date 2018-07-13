499e94dd87b7fb5e24d7c8673a3098ecd6aea1ac
Jerry.Jheng <jerry.jheng@infinitechain.io>
2018-07-13 17:16:15 +0800

    Add serverMetadataHash and clientMetadataHash.

--- gringotts/contracts/ChallengedLib.sol
+++ gringotts/contracts/ChallengedLib.sol
@@ -69,7 +69,7 @@ contract ChallengedLib {
         _parameter[5] = _fee
         _parameter[6] = _nonce
         _parameter[7] = _logID
-        _parameter[8] = _metadataHash
+        _parameter[8] = _clientMetadataHash
 
         ===clientLtxSignature===
         _parameter[9] = _vFromClient
@@ -81,16 +81,17 @@ contract ChallengedLib {
         _parameter[13] = _gsn
         _parameter[14] = _fromBalance
         _parameter[15] = _toBalance
+        _parameter[16] = _serverMetadataHash
 
         ===serverLtxSignature===
-        _parameter[16] = _vFromServer
-        _parameter[17] = _rFromServer
-        _parameter[18] = _sFromServer
+        _parameter[17] = _vFromServer
+        _parameter[18] = _rFromServer
+        _parameter[19] = _sFromServer
 
         ===serverReceiptSignature===
-        _parameter[19] = _vFromServer
-        _parameter[20] = _rFromServer
-        _parameter[21] = _sFromServer
+        _parameter[20] = _vFromServer
+        _parameter[21] = _rFromServer
+        _parameter[22] = _sFromServer
         */
 
         bytes32[] memory bytes32Array = new bytes32[](8);
@@ -101,77 +102,79 @@ contract ChallengedLib {
         bytes32Array[4] = _parameter[5]; // fee
         bytes32Array[5] = _parameter[6]; // nonce
         bytes32Array[6] = _parameter[7]; // logID
-        bytes32Array[7] = _parameter[8]; // metadataHash
+        bytes32Array[7] = _parameter[8]; // clientMetadataHash
         bytes32 hashMsg = Util(utilAddress).hashArray(bytes32Array);
         require(hashMsg == _parameter[0]);
         address signer = Util(utilAddress).verify(hashMsg, uint8(_parameter[9]), _parameter[10], _parameter[11]);
-        bytes32Array = new bytes32[](5);
+        bytes32Array = new bytes32[](6);
         bytes32Array[0] = _parameter[12]; // stageHeight
         bytes32Array[1] = _parameter[13]; // gsn
         bytes32Array[2] = _parameter[0];  // lightTxHash
         bytes32Array[3] = _parameter[14]; // fromBalance
         bytes32Array[4] = _parameter[15]; // toBalance
+        bytes32Array[5] = _parameter[16]; // serverMetadata
         hashMsg = Util(utilAddress).hashArray(bytes32Array);
-        signer = Util(utilAddress).verify(hashMsg, uint8(_parameter[19]), _parameter[20], _parameter[21]);
+        signer = Util(utilAddress).verify(hashMsg, uint8(_parameter[20]), _parameter[21], _parameter[22]);
         require (signer == owner);
         _;
 
         /*
         =============================Receipt2=======================================
-        _parameter[22] = _lightTxHash
+        _parameter[23] = _lightTxHash
 
         ===lightTxData===
-        _parameter[23] = _from
-        _parameter[24] = _to
-        _parameter[25] = _assetID
-        _parameter[26] = _value
-        _parameter[27] = _fee
-        _parameter[28] = _nonce
-        _parameter[29] = _logID
-        _parameter[30] = _metadataHash
+        _parameter[24] = _from
+        _parameter[25] = _to
+        _parameter[26] = _assetID
+        _parameter[27] = _value
+        _parameter[28] = _fee
+        _parameter[29] = _nonce
+        _parameter[30] = _logID
+        _parameter[31] = clientMetadataHash
 
         ===clientLtxSignature===
-        _parameter[31] = _vFromClient
-        _parameter[32] = _rFromClient
-        _parameter[33] = _sFromClient
+        _parameter[32] = _vFromClient
+        _parameter[33] = _rFromClient
+        _parameter[34] = _sFromClient
 
         ===receiptData===
-        _parameter[34] = _stageHeight
-        _parameter[35] = _gsn
-        _parameter[36] = _fromBalance
-        _parameter[37] = _toBalance
-
+        _parameter[35] = _stageHeight
+        _parameter[36] = _gsn
+        _parameter[37] = _fromBalance
+        _parameter[38] = _toBalance
+        _parameter[39] = _serverMetadataHash
         ===serverLtxSignature===
-        _parameter[38] = _vFromServer
-        _parameter[39] = _rFromServer
-        _parameter[40] = _sFromServer
+        _parameter[40] = _vFromServer
+        _parameter[41] = _rFromServer
+        _parameter[42] = _sFromServer
 
         ===serverReceiptSignature===
-        _parameter[41] = _vFromServer
-        _parameter[42] = _rFromServer
-        _parameter[43] = _sFromServer
+        _parameter[43] = _vFromServer
+        _parameter[44] = _rFromServer
+        _parameter[45] = _sFromServer
         */
         if (_parameter.length == 44) {
             bytes32Array = new bytes32[](8);
-            bytes32Array[0] = _parameter[23]; // from
-            bytes32Array[1] = _parameter[24]; // to
-            bytes32Array[2] = _parameter[25]; // assetID
-            bytes32Array[3] = _parameter[26]; // value
-            bytes32Array[4] = _parameter[27]; // fee
-            bytes32Array[5] = _parameter[28]; // nonce
-            bytes32Array[6] = _parameter[29]; // logID
-            bytes32Array[7] = _parameter[30]; // metadataHash
+            bytes32Array[0] = _parameter[24]; // from
+            bytes32Array[1] = _parameter[25]; // to
+            bytes32Array[2] = _parameter[26]; // assetID
+            bytes32Array[3] = _parameter[27]; // value
+            bytes32Array[4] = _parameter[28]; // fee
+            bytes32Array[5] = _parameter[29]; // nonce
+            bytes32Array[6] = _parameter[30]; // logID
+            bytes32Array[7] = _parameter[31]; // clientMetadataHash
             hashMsg = Util(utilAddress).hashArray(bytes32Array);
-            require(hashMsg == _parameter[22]);
-            signer = Util(utilAddress).verify(hashMsg, uint8(_parameter[31]), _parameter[32], _parameter[33]);
-            bytes32Array = new bytes32[](5);
+            require(hashMsg == _parameter[23]);
+            signer = Util(utilAddress).verify(hashMsg, uint8(_parameter[32]), _parameter[33], _parameter[34]);
+            bytes32Array = new bytes32[](6);
             bytes32Array[0] = _parameter[34]; // stageHeight
             bytes32Array[1] = _parameter[35]; // gsn
             bytes32Array[2] = _parameter[22]; // lightTxHash
             bytes32Array[3] = _parameter[36]; // fromBalance
             bytes32Array[4] = _parameter[37]; // toBalance
+            bytes32Array[5] = _parameter[38]; // serverMetadataHash
             hashMsg = Util(utilAddress).hashArray(bytes32Array);
-            signer = Util(utilAddress).verify(hashMsg, uint8(_parameter[41]), _parameter[42], _parameter[43]);
+            signer = Util(utilAddress).verify(hashMsg, uint8(_parameter[43]), _parameter[44], _parameter[45]);
             require (signer == owner);
             _;
         }
@@ -191,45 +194,45 @@ contract ChallengedLib {
     }
     
     function challengedRepeatedGSN (bytes32[] _parameter) isSigValid (_parameter) public {
-        require (uint256(_parameter[13]) == uint256(_parameter[35]));// compare gsn
-        stages[uint256(_parameter[12])].challengedRepeatedGSNList[_parameter[22]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[22]], true, false);
-        stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[22]);
-        emit Challenge (1, bytes32(msg.sender), _parameter[22]);
+        require (uint256(_parameter[13]) == uint256(_parameter[36]));// compare gsn
+        stages[uint256(_parameter[12])].challengedRepeatedGSNList[_parameter[23]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[23]], true, false);
+        stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[23]);
+        emit Challenge (1, bytes32(msg.sender), _parameter[23]);
     }
 
     function challengedWrongBalance (bytes32[] _parameter) isSigValid (_parameter) public {
-        if (address(_parameter[23]) == address(_parameter[1])) {
-            if ((uint256(_parameter[36]) + uint256(_parameter[26])) != uint256(_parameter[14])) {
-                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[22]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[22]], true, false);
-                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[22]);
-                emit Challenge (2, bytes32(msg.sender), _parameter[22]);
-            }
-        } else if (address(_parameter[23]) == address(_parameter[2])) {
-            if ((uint256(_parameter[36]) + uint256(_parameter[26])) != uint256(_parameter[15])) {
-                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[22]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[22]], true, false);
-                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[22]);
-                emit Challenge (2, bytes32(msg.sender), _parameter[22]);
-            }
-        } else if (address(_parameter[24]) == address(_parameter[1])) {
-            if ((uint256(_parameter[36]) - uint256(_parameter[26])) != uint256(_parameter[14])) {
-                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[22]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[22]], true, false);
-                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[22]);
-                emit Challenge (2, bytes32(msg.sender), _parameter[22]);
+        if (address(_parameter[24]) == address(_parameter[1])) {
+            if ((uint256(_parameter[37]) + uint256(_parameter[27])) != uint256(_parameter[14])) {
+                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[23]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[23]], true, false);
+                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[23]);
+                emit Challenge (2, bytes32(msg.sender), _parameter[23]);
             }
         } else if (address(_parameter[24]) == address(_parameter[2])) {
-            if ((uint256(_parameter[36]) - uint256(_parameter[26])) != uint256(_parameter[15])) {
-                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[22]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[22]], true, false);
-                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[22]);
-                emit Challenge (2, bytes32(msg.sender), _parameter[22]);
+            if ((uint256(_parameter[37]) + uint256(_parameter[27])) != uint256(_parameter[15])) {
+                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[23]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[23]], true, false);
+                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[23]);
+                emit Challenge (2, bytes32(msg.sender), _parameter[23]);
+            }
+        } else if (address(_parameter[25]) == address(_parameter[1])) {
+            if ((uint256(_parameter[37]) - uint256(_parameter[27])) != uint256(_parameter[14])) {
+                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[23]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[23]], true, false);
+                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[23]);
+                emit Challenge (2, bytes32(msg.sender), _parameter[23]);
+            }
+        } else if (address(_parameter[25]) == address(_parameter[2])) {
+            if ((uint256(_parameter[37]) - uint256(_parameter[27])) != uint256(_parameter[15])) {
+                stages[uint256(_parameter[12])].challengedWrongBalanceList[_parameter[23]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[23]], true, false);
+                stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[23]);
+                emit Challenge (2, bytes32(msg.sender), _parameter[23]);
             }
         }
     }
 
     function challengedSkippedGSN (bytes32[] _parameter) isSigValid (_parameter) public {
-        require (uint256(_parameter[35]) - uint256(_parameter[13]) != 1);
-        stages[uint256(_parameter[12])].challengedSkippedGSNList[_parameter[22]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[22]], true, false);
-        stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[22]);
-        emit Challenge (3, bytes32(msg.sender), _parameter[22]);
+        require (uint256(_parameter[36]) - uint256(_parameter[13]) != 1);
+        stages[uint256(_parameter[12])].challengedSkippedGSNList[_parameter[23]] = ChallengedLib.ChallengedInfo(msg.sender, [_parameter[0], _parameter[23]], true, false);
+        stages[uint256(_parameter[12])].challengedLightTxHashes.push(_parameter[23]);
+        emit Challenge (3, bytes32(msg.sender), _parameter[23]);
     }
 
     // function challengedExistedProof (bytes32[] _parameter) isSigValid (_parameter) public {
