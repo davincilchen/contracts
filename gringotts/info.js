d3bc6ac31e1c50ec23e71d799b7f5dfad1d80e91
Ben Lu <coopermilk123@gmail.com>
2018-07-22 17:36:32 +0800

    Support standalone deployment process

--- gringotts/info.js
+++ gringotts/info.js
@@ -1,10 +1,43 @@
 let EthUtils = require('ethereumjs-util');
 let env = require('./env');
 
-const privatekey = env.privateKey;
-const publickey = '0x' + EthUtils.privateToPublic('0x' + privatekey).toString('hex');
-const account = '0x' + EthUtils.pubToAddress(publickey).toString('hex');
+let devnet = env.devnet;
+let testnet = env.testnet;
+let mainnet = env.mainnet;
 
-console.log('Private key: ' + privatekey);
-console.log('Public key: ' + publickey);
-console.log('address: ' + account);
+console.log('----------------Devnet-------------------');
+const devPrivatekey = devnet.privateKey;
+if (devPrivatekey) {
+  const devPublickey = '0x' + EthUtils.privateToPublic('0x' + devPrivatekey).toString('hex');
+  const devAccount = '0x' + EthUtils.pubToAddress(devPublickey).toString('hex');
+  console.log('Private key: ' + devPrivatekey);
+  console.log('Public key: ' + devPublickey);
+  console.log('address: ' + devAccount);
+} else {
+  console.log('Not detected.');
+}
+
+
+console.log('----------------Testnet-------------------');
+const testnetPrivatekey = testnet.privateKey;
+if (testnetPrivatekey) {
+  const testnetPublickey = '0x' + EthUtils.privateToPublic('0x' + testnetPrivatekey).toString('hex');
+  const testnetAccount = '0x' + EthUtils.pubToAddress(testnetPublickey).toString('hex');
+  console.log('Private key: ' + testnetPrivatekey);
+  console.log('Public key: ' + testnetPublickey);
+  console.log('address: ' + testnetAccount);
+} else {
+  console.log('Not detected.');
+}
+
+console.log('----------------Mainnet-------------------');
+const mainnetPrivatekey = mainnet.privateKey;
+if (mainnetPrivatekey) {
+  const mainnetPublickey = '0x' + EthUtils.privateToPublic('0x' + mainnetPrivatekey).toString('hex');
+  const mainnetAccount = '0x' + EthUtils.pubToAddress(mainnetPublickey).toString('hex');
+  console.log('Private key: ' + mainnetPrivatekey);
+  console.log('Public key: ' + mainnetPublickey);
+  console.log('address: ' + mainnetAccount);
+} else {
+  console.log('Not detected.');
+}
--- gringotts/truffle.js
+++ gringotts/truffle.js
@@ -1,47 +1,40 @@
-fs = require('fs');
+let env = require('./env');
+let WalletProvider = require('truffle-wallet-provider');
+let Wallet = require('ethereumjs-wallet');
 
-let dev = {};
-if (fs.existsSync('./env.js')) {
-    let Web3 = require('web3');
-    let EthUtils = require('ethereumjs-util');
-    let env = require('./env');
+let devnet = env.devnet;
+let testnet = env.testnet;
+let mainnet = env.mainnet;
 
-    let web3Url = 'http://' + env.web3Host + ':' + env.web3Port;
-    let web3 = new Web3(new Web3.providers.HttpProvider(web3Url));
-    const account = env.account;
-    web3.personal.unlockAccount(account, env.password);
-    
-    dev = {
-        host: env.web3Host,
-        port: env.web3Port,
-        network_id: '*',
-        from: account,
-        gas: 4700000
-    }
+let devnetWallet = Wallet.fromPrivateKey(new Buffer(devnet.privateKey, 'hex'));
+let devnetProvider = new WalletProvider(devnetWallet, devnet.web3Url);
 
-} else {
-    dev = {
-        host: 'localhost',
-        port: 8545,
-        network_id: '*',
-        gas: 4700000
-    }
-}
+let testnetWallet = Wallet.fromPrivateKey(new Buffer(testnet.privateKey, 'hex'));
+let testnetProvider = new WalletProvider(testnetWallet, testnet.web3Url);
+
+let mainnetWallet = Wallet.fromPrivateKey(new Buffer(mainnet.privateKey, 'hex'));
+let mainnetProvider = new WalletProvider(mainnetWallet, mainnet.web3Url);
 
 module.exports = {
-    solc: {
-        optimizer: {
-            enabled: true,
-            runs: 200
-        }
-    },
-    networks: {
-        development: dev,
-        staging: {
-            host: '<HOST_DOMAIN>',
-            port: '<HOST_PORT>',
-            network_id: '*',
-            from: '<KEY>'
-        }
+  solc: {
+    optimizer: {
+        enabled: true,
+        runs: 200
     }
+  },
+  networks: {
+    development: {
+      provider: devnetProvider,
+      network_id: '*',
+      gas: 4700000
+    },
+    testnet: {
+      provider: testnetProvider,
+      network_id: 4
+    },
+    mainnet: {
+      provider: mainnetProvider,
+      network_id: 1
+    }
+  }
 }
