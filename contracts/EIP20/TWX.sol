148a8326a4d5d7a3fe2516c60028531a86318fdb
doki_chen <doki_chen@misde-MacBook-Pro-2.local>
2018-08-08 14:55:45 +0800

    save supply info

--- contracts/EIP20/TWX.sol
+++ contracts/EIP20/TWX.sol
@@ -4,15 +4,58 @@ import "./Ownable.sol";
 
 contract TWX is EIP20,Ownable {
 //contract TWX is EIP20 {
-	function TWX() EIP20(10**(10+18), "New Taiwan Dollar X", 18, "TWX") public {
-	// EIP20(total supply, name, decimals, symbols)
+
+	enum TokenOP {
+		MINT,
+		BURN
 	}
 
-	function getContractAdd() public onlyOwner constant returns (address){
+
+	struct SupplyInfo { // Struct
+	    uint amount;
+        string log;
+        address sender;
+		uint opCode;
+		//TokenOP opCode;
+        //time
+    }
+
+
+	uint public constant INITIAL_SUPPLY = 10**(10+18);
+	SupplyInfo[] supplyInfo;
+
+
+	function TWX() EIP20(INITIAL_SUPPLY, "New Taiwan Dollar X", 18, "TWX") public {
+	//function TWX() EIP20(10**(10+18), "New Taiwan Dollar X", 18, "TWX") public {
+		// EIP20(total supply, name, decimals, symbols)
+		
+		
+		supplyInfo.push(SupplyInfo({
+			amount: INITIAL_SUPPLY,
+			log: "init",
+			sender: msg.sender,
+			//opCode: MINT
+			opCode: 0
+		}));
+
+
+	}
+
+	function getSupplyInfoCount() public constant returns (uint){
+		return supplyInfo.length;
+	}
+
+
+	// ========== test part ========== //
+	function getContractAddress() public onlyOwner constant returns (address){
 		return this;
 	}
 
-	function getCallAddress() public constant returns (address){
+	function getSenderAddress() public constant returns (address){
 		return msg.sender;
+	}
+
+	function getOwnerAddress() public constant returns (address){
+		return owner;
 	}
 }
